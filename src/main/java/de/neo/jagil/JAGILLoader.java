b5cc990e52315eab30c1eced7b05dc47beb76b13
Keksnet <56829999+Keksnet@users.noreply.github.com>
2021-06-20 20:28:47 +0200

    Added files via upload

---
+++ src/main/java/de/neo/jagil/JAGILLoader.java
@@ -0,0 +1,10 @@
+package de.neo.jagil;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class JAGILLoader extends JavaPlugin {
+	
+	public void onEnable() {
+		this.getLogger().info("JAGIL loaded!");
+	}
+}
